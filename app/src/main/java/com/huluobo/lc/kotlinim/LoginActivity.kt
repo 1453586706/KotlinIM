4679ca6fec482d67e73ba020aef63e6040fdf1c5
liuchang <moroks@163.com>
2020-05-25 16:50:59 +0800

    添加View层跳转功能,添加了loginActivity

---
+++ app/src/main/java/com/huluobo/lc/kotlinim/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.huluobo.lc.kotlinim
+
+import com.huluobo.lc.kotlinim.base.BaseActivity
+
+/**
+ * @author Lc
+ * @description:
+ * @date :2020/5/25 16:40
+ */
+class LoginActivity :BaseActivity(){
+    override fun getLayoutResId(): Int =R.layout.activity_login
+
+}
--- app/src/main/java/com/huluobo/lc/kotlinim/SplashActivity.kt
+++ app/src/main/java/com/huluobo/lc/kotlinim/SplashActivity.kt
@@ -1,14 +1,39 @@
 package com.huluobo.lc.kotlinim
 
+import android.os.Handler
 import com.huluobo.lc.kotlinim.base.BaseActivity
+import com.huluobo.lc.kotlinim.contract.SplashContract
+import org.jetbrains.anko.startActivity
 
 /**
  * @author Lc
  * @description:
  * @date :2020/5/25 16:09
  */
-class SplashActivity : BaseActivity() {
+class SplashActivity : BaseActivity(), SplashContract.View {
+    companion object {
+        const val DELAY = 2000L
+    }
+
+    private val handler by lazy {
+        Handler()
+    }
+
     override fun getLayoutResId(): Int =
         R.layout.activity_splash
+
+    //1.如果没有登录,延时2S,跳转登录页面
+    override fun onNotLoggedIn() {
+        handler.postDelayed({
+            startActivity<LoginActivity>()
+            finish()
+        }, DELAY)
+    }
+
+    //2.如果已经登录,则跳转到主页面
+    override fun onLoggedIn() {
+        startActivity<MainActivity>()
+        finish()
+    }
 
 }
