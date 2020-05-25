ec619771aff703b8fffd0548e509e27fde2beb02
liuchang <moroks@163.com>
2020-05-25 16:15:51 +0800

    添加BaseActivity,BaseFragment

---
+++ app/src/main/java/com/huluobo/lc/kotlinim/SplashActivity.kt
@@ -0,0 +1,14 @@
+package com.huluobo.lc.kotlinim
+
+import com.huluobo.lc.kotlinim.base.BaseActivity
+
+/**
+ * @author Lc
+ * @description:
+ * @date :2020/5/25 16:09
+ */
+class SplashActivity : BaseActivity() {
+    override fun getLayoutResId(): Int =
+        R.layout.activity_splash
+
+}
