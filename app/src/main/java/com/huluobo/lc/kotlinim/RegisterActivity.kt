ee9ba677a95f65512f6cda0ffd6fbbf902f99b49
liuchang <moroks@163.com>
2020-05-26 16:48:36 +0800

    注册页面的搭建,注册协议搭建

---
+++ app/src/main/java/com/huluobo/lc/kotlinim/RegisterActivity.kt
@@ -0,0 +1,7 @@
+package com.huluobo.lc.kotlinim
+
+import com.huluobo.lc.kotlinim.base.BaseActivity
+
+class RegisterActivity : BaseActivity() {
+    override fun getLayoutResId(): Int = R.layout.activity_register
+}
