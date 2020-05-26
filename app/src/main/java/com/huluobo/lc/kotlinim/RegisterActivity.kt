2f058eae03a6a006ecddcb41dfd8e01da4b96f25
liuchang <moroks@163.com>
2020-05-26 17:05:34 +0800

    注册页面View层的实现

--- app/src/main/java/com/huluobo/lc/kotlinim/RegisterActivity.kt
+++ app/src/main/java/com/huluobo/lc/kotlinim/RegisterActivity.kt
@@ -1,7 +1,35 @@
 package com.huluobo.lc.kotlinim
 
 import com.huluobo.lc.kotlinim.base.BaseActivity
+import com.huluobo.lc.kotlinim.contract.RegisterContract
+import kotlinx.android.synthetic.main.activity_register.*
+import org.jetbrains.anko.toast
 
-class RegisterActivity : BaseActivity() {
+class RegisterActivity : BaseActivity(), RegisterContract.View {
     override fun getLayoutResId(): Int = R.layout.activity_register
+    override fun onUserNameError() {
+        userName.error = getString(R.string.user_name_error)
+    }
+
+    override fun onPasswordError() {
+        password.error = getString(R.string.password_error)
+    }
+
+    override fun onConfirmPasswordError() {
+        confirmPassword.error = getString(R.string.confirm_password_error)
+    }
+
+    override fun onStartRegister() {
+        showProgress(getString(R.string.registering))
+    }
+
+    override fun onRegisterSuccess() {
+        dimissProgress()
+        finish()
+    }
+
+    override fun onRegisterFailed() {
+        dimissProgress()
+        toast(R.string.register_failed)
+    }
 }
