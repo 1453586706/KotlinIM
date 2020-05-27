3c4b1bbd6878679ef124c4709b93d90a9d2e06da
liuchang <moroks@163.com>
2020-05-27 14:19:17 +0800

    注册到Bmob

--- app/src/main/java/com/huluobo/lc/kotlinim/presenter/RegisterPresenter.kt
+++ app/src/main/java/com/huluobo/lc/kotlinim/presenter/RegisterPresenter.kt
@@ -1,5 +1,8 @@
 package com.huluobo.lc.kotlinim.presenter
 
+import cn.bmob.v3.BmobUser
+import cn.bmob.v3.exception.BmobException
+import cn.bmob.v3.listener.SaveListener
 import com.huluobo.lc.kotlinim.contract.RegisterContract
 import com.huluobo.lc.kotlinim.extentions.isValidPassword
 import com.huluobo.lc.kotlinim.extentions.isValidUserName
@@ -18,10 +21,29 @@ class RegisterPresenter(val view: RegisterContract.View) : RegisterContract.Pres
                 if (password == confirmPassword) {
                     //密码和确认密码一致
                     view.onStartRegister()
-                    //开始注册
-
+                    //开始注册 //正常公司中的app需要注册到公司的后台服务器,因为咱们没有后台,所以注册到Bomb模拟后台数据
+                    registerBmob(userName, password)
                 } else view.onConfirmPasswordError()
             } else view.onPasswordError()
         } else view.onUserNameError()
+    }
+
+    private fun registerBmob(userName: String, password: String) {
+        val bmobUser = BmobUser()
+        bmobUser.username = userName
+        bmobUser.setPassword(password)
+
+        bmobUser.signUp(object : SaveListener<BmobUser>() {
+            override fun done(bmobUser: BmobUser?, e: BmobException?) {
+                if (e == null) {
+                    //注册成功
+                    //注册到环信,(公司流程)从服务器直接注册到环信服务器,返回给app是成功或者失败的消息
+
+                } else {
+                    //注册失败
+                    view.onRegisterFailed()
+                }
+            }
+        })
     }
 }
