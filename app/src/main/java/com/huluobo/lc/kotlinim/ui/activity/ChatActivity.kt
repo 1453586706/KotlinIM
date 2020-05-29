28ec696b7a422b3d15e6c7623ffefedab12d2cdd
liuchang <moroks@163.com>
2020-05-29 16:32:06 +0800

    聊天activity布局的实现

--- app/src/main/java/com/huluobo/lc/kotlinim/ui/activity/ChatActivity.kt
+++ app/src/main/java/com/huluobo/lc/kotlinim/ui/activity/ChatActivity.kt
@@ -1,14 +1,53 @@
 package com.huluobo.lc.kotlinim.ui.activity
 
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import androidx.core.widget.addTextChangedListener
 import com.huluobo.lc.kotlinim.R
 import com.huluobo.lc.kotlinim.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_chat.*
+import kotlinx.android.synthetic.main.header.*
 
 /**
  * @author Lc
  * @description:
  * @date :2020/5/28 10:33
  */
-class ChatActivity :BaseActivity(){
+class ChatActivity : BaseActivity() {
     override fun getLayoutResId(): Int = R.layout.activity_chat
+
+    override fun init() {
+        super.init()
+        initHeader()
+        initEditText()
+    }
+
+    private fun initEditText() {
+        edit.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                //如果用户输入的文本长度大于0,发送按钮enable
+                send.isEnabled = !s.isNullOrEmpty()
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            }
+
+        })
+    }
+
+    private fun initHeader() {
+        back.visibility = View.VISIBLE
+        back.setOnClickListener { finish() }
+
+        //获取聊天的用户名
+        val username = intent.getStringExtra("username")
+        val titleString = String.format(getString(R.string.chat_title), username)
+        headerTitle.text = titleString
+    }
+
 
 }
